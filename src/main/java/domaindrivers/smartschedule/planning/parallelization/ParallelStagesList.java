03a87f18e3072ec0410b8f32e95b933d90937280
jpilimon <jakub.pilimon@gmail.com>
2024-01-02 17:00:12 +0200

    task to implement taking into account shared resource while parallelization

--- src/main/java/domaindrivers/smartschedule/planning/parallelization/ParallelStagesList.java
+++ src/main/java/domaindrivers/smartschedule/planning/parallelization/ParallelStagesList.java
@@ -6,22 +6,11 @@ import java.util.stream.Stream;
 
 public record ParallelStagesList(List<ParallelStages> all) {
 
-    public static ParallelStagesList empty() {
-        return new ParallelStagesList(List.of());
-    }
-
     public String print() {
         return all.stream()
                 .map(ParallelStages::print)
                 .collect(Collectors.joining(" | "));
     }
 
-    public ParallelStagesList add(ParallelStages newParallelStages) {
-        List<ParallelStages> result =
-                Stream
-                        .concat(this.all.stream(), Stream.of(newParallelStages))
-                        .collect(Collectors.toList());
-        return new ParallelStagesList(result);
-    }
 }
 
