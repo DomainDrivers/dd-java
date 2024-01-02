647d409e562f4fe695dda42e0ce049f911d88f8e
jpilimon <jakub.pilimon@gmail.com>
2024-01-02 17:01:14 +0200

    implemented task to implement taking into account shared resource while parallelization

--- src/main/java/domaindrivers/smartschedule/planning/parallelization/StagesToNodes.java
+++ src/main/java/domaindrivers/smartschedule/planning/parallelization/StagesToNodes.java
@@ -15,9 +15,29 @@ class StagesToNodes {
         for (int i = 0; i < stages.size(); i++) {
             Stage stage = stages.get(i);
             result = explicitDependencies(stage, result);
+            result = sharedResources(stage, stages.stream().skip(i + 1).collect(Collectors.toList()), result);
         }
 
         return new Nodes(new HashSet<>(result.values()));
+    }
+
+    private Map<String, Node> sharedResources(Stage stage, List<Stage> with, Map<String, Node> result) {
+        for (Stage other : with) {
+            if (!stage.name().equals(other.name())) {
+                if (!Collections.disjoint(stage.resources(), other.resources())) {
+                    if (other.resources().size() > stage.resources().size()) {
+                        Node node = result.get(stage.name());
+                        node = node.dependsOn(result.get(other.name()));
+                        result.put(stage.name(), node);
+                    } else {
+                        Node node = result.get(other.name());
+                        node = node.dependsOn(result.get(stage.name()));
+                        result.put(other.name(), node);
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     private Map<String, Node> explicitDependencies(Stage stage, Map<String, Node> result) {
